1c707f90cc83520250cb86c9df99dc3b61ea12c8
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-24 01:22:03 +0200

    move to js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 const childProcess = require('child_process');
 
 const { execSync } = childProcess;
 
+const mountPoint = '/var/tmp';
+
 const app = () => {
-  const output = execSync('docker -v').toString();
-  // console.log(process.env.ACTIONS_RUNTIME_URL);
-  console.log(output);
+  fs.mkdirSync(path.join(mountPoint, 'source'));
+
+  const output1 = execSync(
+    `docker run -v ${mountPoint}:/mnt hexletprojects/css_l1_moon_project:release bash -c 'cp -r /project/. /mnt/source && rm -rf /mnt/source/code'`,
+  ).toString();
+  console.log(output1);
+
+  fs.mkdirSync(path.join(mountPoint, 'source', 'code'));
+
+  const output2 = execSync('cp -r `pwd`/. $MOUNT_POINT/source/code').toString();
+  console.log(output2);
+
+  const output3 = execSync(
+    'docker tag hexletprojects/css_l1_moon_project:release source_development:latest',
+  ).toString();
+  console.log(output3);
+
+  const output4 = execSync(
+    `cd ${mountPoint}/source && docker-compose run development make setup test lint`,
+  ).toString();
+  console.log(output4);
 };
 
 app();
