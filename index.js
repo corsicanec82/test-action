7510185a7a0d50af0329e358656d8a80c6747463
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-24 00:53:20 +0200

    test sh flow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { execSync } = childProcess;
 
 const app = () => {
   // console.log(process.env.ACTIONS_RUNTIME_URL);
-  const output = execSync('flow.sh');
+  const output = execSync('flow.sh').toString();
   console.log(output);
 };
 
