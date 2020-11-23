d04feb29e8f0092fa845207ee49e9b39ba71fa2b
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-24 01:01:27 +0200

    fix sh flow

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const childProcess = require('child_process');
 
-const { execSync } = childProcess;
+const { spawn } = childProcess;
 
 const app = () => {
   // console.log(process.env.ACTIONS_RUNTIME_URL);
-  const output = execSync('flow.sh').toString();
-  console.log(output);
+  const bash = spawn('bash', ['flow.sh']);
+
+  bash.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  bash.on('exit', (code) => {
+    console.log(`exit`);
+  });
 };
 
 app();
