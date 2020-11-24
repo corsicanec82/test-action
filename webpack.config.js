1268e95def5453addc339f25b7a0d53c66e698bd
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-24 13:35:47 +0200

    add bundle

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// const path = require('path');
+
+module.exports = {
+  output: {
+    filename: 'index.js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
