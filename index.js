d06ef7188995008a81e0ca3f69ba7e7ee63ed07b
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-24 12:07:00 +0200

    improve solution

--- index.js
+++ index.js
@@ -9,25 +9,27 @@ const mountPoint = '/var/tmp';
 const app = () => {
   fs.mkdirSync(path.join(mountPoint, 'source'));
 
-  const output1 = execSync(
+  execSync(
     `docker run -v ${mountPoint}:/mnt hexletprojects/css_l1_moon_project:release bash -c 'cp -r /project/. /mnt/source && rm -rf /mnt/source/code'`,
-  ).toString();
-  console.log(output1);
+    { stdio: 'inherit' },
+  );
 
   fs.mkdirSync(path.join(mountPoint, 'source', 'code'));
 
-  const output2 = execSync(`cp -r ${__dirname}/. ${mountPoint}/source/code`).toString();
-  console.log(output2);
+  execSync(
+    `cp -r ${__dirname}/. ${mountPoint}/source/code`,
+    { stdio: 'inherit' },
+  );
 
-  const output3 = execSync(
+  execSync(
     'docker tag hexletprojects/css_l1_moon_project:release source_development:latest',
-  ).toString();
-  console.log(output3);
+    { stdio: 'inherit' },
+  );
 
-  const output4 = execSync(
+  execSync(
     `cd ${mountPoint}/source && docker-compose run development make setup test lint`,
-  ).toString();
-  console.log(output4);
+    { stdio: 'inherit' },
+  );
 };
 
 app();
