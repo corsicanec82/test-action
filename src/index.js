3a3a5b7c39ff2eaabf4ff0557f72d4ee25c4a8df
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-11-25 12:47:49 +0200

    refactoring action

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const childProcess = require('child_process');
+// const childProcess = require('child_process');
 const artifact = require('@actions/artifact');
 const core = require('@actions/core');
 const io = require('@actions/io');
 const exec = require('@actions/exec');
 
-const { execSync } = childProcess;
+// const { execSync } = childProcess;
 
-const mountPoint = '/var/tmp';
+const mountPoint = path.join('/', 'var', 'tmp');
+const buildPath = path.join(mountPoint, 'source');
+const codePath = path.join(buildPath, 'code');
+const projectPath = process.cwd();
 
 const diffpath = path.join(
   mountPoint,
@@ -44,48 +47,44 @@ const uploadArtifacts = async () => {
   const artifactClient = artifact.create();
   const artifactName = 'test-results';
   await artifactClient.uploadArtifact(artifactName, filepaths, diffpath);
-  console.log('Download snapshots from Artifacts');
+  console.log('Download snapshots = require(Artifacts');
 };
 
 const app = async () => {
-  core.debug('Inside try block');
-  core.warning('myInput was not set');
-  core.info('Output to the actions build log');
-  // 3/4 bit
-  core.info('\u001b[35mThis foreground will be magenta');
+  // core.debug('Inside try block');
+  // core.warning('myInput was not set');
+  // core.info('Output to the actions build log');
+  // // 3/4 bit
+  // core.info('\u001b[35mThis foreground will be magenta');
 
-  // 8 bit
-  core.info('\u001b[38;5;6mThis foreground will be cyan');
+  // // 8 bit
+  // core.info('\u001b[38;5;6mThis foreground will be cyan');
 
-  // 24 bit
-  core.info('\u001b[38;2;255;0;0mThis foreground will be bright red');
-  // Background colors:
+  // // 24 bit
+  // core.info('\u001b[38;2;255;0;0mThis foreground will be bright red');
+  // // Background colors:
 
-  // 3/4 bit
-  core.info('\u001b[43mThis background will be yellow');
+  // // 3/4 bit
+  // core.info('\u001b[43mThis background will be yellow');
 
-  // 8 bit
-  core.info('\u001b[48;5;6mThis background will be cyan');
+  // // 8 bit
+  // core.info('\u001b[48;5;6mThis background will be cyan');
 
-  // 24 bit
-  core.info('\u001b[48;2;255;0;0mThis background will be bright red');
-  // Special styles:
+  // // 24 bit
+  // core.info('\u001b[48;2;255;0;0mThis background will be bright red');
+  // // Special styles:
 
-  core.info('\u001b[1mBold text');
-  core.info('\u001b[3mItalic text');
-  core.info('\u001b[4mUnderlined text');
-  core.error('Error action may still succeed though');
-
-  const buildPath = path.join(mountPoint, 'source');
-  const codePath = path.join(buildPath, 'code');
-  const projectPath = process.cwd();
+  // core.info('\u001b[1mBold text');
+  // core.info('\u001b[3mItalic text');
+  // core.info('\u001b[4mUnderlined text');
+  // core.error('Error action may still succeed though');
 
   await io.mkdirP(buildPath);
   await exec.exec(
     `docker run -v ${mountPoint}:/mnt hexletprojects/css_l1_moon_project:release bash -c "cp -r /project/. /mnt/source && rm -rf /mnt/source/code"`,
   );
   await io.mkdirP(codePath);
-  await io.cp(projectPath, codePath, { recursive: true });
+  await io.cp(`${projectPath}/.`, codePath, { recursive: true });
   await exec.exec(`ls -la ${codePath}`);
 
   // fs.mkdirSync(path.join(mountPoint, 'source'));
