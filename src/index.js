4fedf2034c49f7f2f82730963b86ffa9fc4de347
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-02-04 15:03:22 +0200

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const exec = require('@actions/exec');
 const { execSync } = require('child_process');
 const get = require('lodash/get');
 const chalk = require('chalk');
+const os = require('os');
 
 const apiUrl = 'https://hexlet.io/api/github_workflow/v1/project/';
 
@@ -54,80 +55,80 @@ const uploadArtifacts = async () => {
   core.warning('The link is above the output window.');
 };
 
-const app = async () => {
+const app = () => {
   core.info('Test string');
-  console.log(chalk.blue('Hello world!'));
+  process.stdout.write(chalk.blue('sdsdsdsd') + os.EOL);
   core.info('Test string');
-  return;
-  core.info('Checking the possibility of starting testing...');
-  // Get readiness for checking repo
-  const urlCheck = new URL('ready_to_check/', apiUrl);
-  urlCheck.searchParams.set('github_repository', 'mom4uk/backend-project-lvl2');
-  // urlCheck.searchParams.set('github_repository', githubRepository);
-  const responseCheck = execSync(`curl -s ${urlCheck.toString()}`);
-  try {
-    const result = JSON.parse(responseCheck.toString());
-    if (!result) {
-      core.error('Hexlet check will run after finish the last project step.');
-      process.exit(1);
-    }
-  } catch (e) {
-    core.error('Project or user not found!');
-    process.exit(1);
-  }
 
-  // Get project base image name
-  let imageName;
-  const urlProject = new URL(apiUrl);
-  urlProject.searchParams.set('github_repository', 'mom4uk/backend-project-lvl2');
-  // urlProject.searchParams.set('github_repository', githubRepository);
-  const responseProject = execSync(`curl -s ${urlProject.toString()}`);
-  try {
-    const data = JSON.parse(responseProject.toString());
-    imageName = get(data, 'project.image_name');
-    if (!imageName) {
-      core.error('Image name is not defined!');
-      process.exit(1);
-    }
-  } catch (e) {
-    core.error('An error occurred getting the image name!');
-    process.exit(1);
-  }
-  core.info('\u001b[38;5;6mChecking completed.');
+  // core.info('Checking the possibility of starting testing...');
+  // // Get readiness for checking repo
+  // const urlCheck = new URL('ready_to_check/', apiUrl);
+  // urlCheck.searchParams.set('github_repository', 'mom4uk/backend-project-lvl2');
+  // // urlCheck.searchParams.set('github_repository', githubRepository);
+  // const responseCheck = execSync(`curl -s ${urlCheck.toString()}`);
+  // try {
+  //   const result = JSON.parse(responseCheck.toString());
+  //   if (!result) {
+  //     core.error('Hexlet check will run after finish the last project step.');
+  //     process.exit(1);
+  //   }
+  // } catch (e) {
+  //   core.error('Project or user not found!');
+  //   process.exit(1);
+  // }
 
-  core.info('Preparing to start testing. It can take some time. Please wait...');
-  // Create build directory
-  await io.mkdirP(buildPath);
+  // // Get project base image name
+  // let imageName;
+  // const urlProject = new URL(apiUrl);
+  // urlProject.searchParams.set('github_repository', 'mom4uk/backend-project-lvl2');
+  // // urlProject.searchParams.set('github_repository', githubRepository);
+  // const responseProject = execSync(`curl -s ${urlProject.toString()}`);
+  // try {
+  //   const data = JSON.parse(responseProject.toString());
+  //   imageName = get(data, 'project.image_name');
+  //   if (!imageName) {
+  //     core.error('Image name is not defined!');
+  //     process.exit(1);
+  //   }
+  // } catch (e) {
+  //   core.error('An error occurred getting the image name!');
+  //   process.exit(1);
+  // }
+  // core.info('\u001b[38;5;6mChecking completed.');
 
-  // Copy original project files
-  await exec.exec(
-    `docker run -v ${mountPoint}:/mnt hexletprojects/css_l1_moon_project:release bash -c "cp -r /project/. /mnt/source && rm -rf /mnt/source/code"`,
-    [],
-    { silent: true },
-  );
+  // core.info('Preparing to start testing. It can take some time. Please wait...');
+  // // Create build directory
+  // await io.mkdirP(buildPath);
 
-  // Create user code directory
-  await io.mkdirP(codePath);
+  // // Copy original project files
+  // await exec.exec(
+  //   `docker run -v ${mountPoint}:/mnt hexletprojects/css_l1_moon_project:release bash -c "cp -r /project/. /mnt/source && rm -rf /mnt/source/code"`,
+  //   [],
+  //   { silent: true },
+  // );
 
-  // Copy user project to build directory
-  await io.cp(`${projectPath}/.`, codePath, { recursive: true });
+  // // Create user code directory
+  // await io.mkdirP(codePath);
 
-  // Create a tags
-  await exec.exec('docker tag hexletprojects/css_l1_moon_project:release source_development:latest', [], { silent: true });
+  // // Copy user project to build directory
+  // await io.cp(`${projectPath}/.`, codePath, { recursive: true });
 
-  // Build images
-  await exec.exec('docker-compose', ['build'], { cwd: buildPath, silent: true });
-  core.info('\u001b[38;5;6mPreparing completed.');
+  // // Create a tags
+  // await exec.exec('docker tag hexletprojects/css_l1_moon_project:release source_development:latest', [], { silent: true });
 
-  try {
-    // Run setup, tests, lint
-    await exec.exec('docker-compose', ['run', 'development', 'make', 'setup', 'test', 'lint'], { cwd: buildPath });
-  } catch (e) {
-    // Upload artifacts
-    await uploadArtifacts();
-    core.error('Testing failed. See the output.');
-    process.exit(1);
-  }
+  // // Build images
+  // await exec.exec('docker-compose', ['build'], { cwd: buildPath, silent: true });
+  // core.info('\u001b[38;5;6mPreparing completed.');
+
+  // try {
+  //   // Run setup, tests, lint
+  //   await exec.exec('docker-compose', ['run', 'development', 'make', 'setup', 'test', 'lint'], { cwd: buildPath });
+  // } catch (e) {
+  //   // Upload artifacts
+  //   await uploadArtifacts();
+  //   core.error('Testing failed. See the output.');
+  //   process.exit(1);
+  // }
 };
 
 app();
